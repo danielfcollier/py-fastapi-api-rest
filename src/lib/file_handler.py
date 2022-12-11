b6b2be4999851c51881e4aaace77f957a5e6a8ec
danielfcollier <danielfcollier@gmail.com>
2022-12-11 09:51:58 -0300

    File database just for development.

---
+++ src/lib/file_handler.py
@@ -0,0 +1,11 @@
+import json
+
+def read_json(filepath):
+  with open(filepath, "r") as json_file:
+    data = json.load(json_file)
+
+  return data
+
+def write_json(filepath, data):
+  with open(filepath, "w") as json_file:
+    json.dump(data, json_file)
